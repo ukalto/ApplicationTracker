febdee93ebc0d0460ea770a49d981be8f75dd515
ukalto <maxi.gaber@gmail.com>
2023-05-31 18:26:56 +0200

    added more columns

--- main.py
+++ main.py
@@ -5,39 +5,55 @@ import os.path
 
 
 class JobApplication:
-    def __init__(self, company, job_position, application_sent, job_source):
+    def __init__(self, company, job_position, application_sent, job_source, answer, interview, interview_datetime, reason_for_no):
         self.company = company
         self.job_position = job_position
         self.application_sent = application_sent
         self.job_source = job_source
+        self.answer = answer
+        self.interview = interview
+        self.interview_datetime = interview_datetime
+        self.reason_for_no = reason_for_no
 
     def to_dict(self):
         return {
             'company': self.company,
             'job_position': self.job_position,
             'application_sent': self.application_sent,
-            'job_source': self.job_source
+            'job_source': self.job_source,
+            'answer': self.answer,
+            'interview': self.interview,
+            'interview_datetime': self.interview_datetime,
+            'reason_for_no': self.reason_for_no
         }
 
 
-data = []
+file_exists = os.path.isfile('applications.csv')
 
 
 def main():
-    try:
-        while True:
+    while True:
+        try:
             company = input("Company: ")
             jop_position = input("Job Position: ")
             application_sent = date.today()
             job_source = input("Website/Source of job: ")
-            application = JobApplication(company, jop_position, application_sent, job_source)
-            data.append(application.to_dict())
-    except KeyboardInterrupt:
-        file_exists = os.path.isfile('applications.csv')
-        df = pd.DataFrame(data)
-        df.to_csv('applications.csv', index=False, mode='a', header=not file_exists)
-        print("\n---Finished---")
-        time.sleep(1)
+            answer = 'neither'
+            interview = 'neither'
+            interview_datetime = 'null'
+            reason_for_no = 'null'
+            application = JobApplication(company, jop_position, application_sent, job_source, answer, interview, interview_datetime, reason_for_no)
+            data = [application.to_dict()]
+            df = pd.DataFrame(data)
+            df.to_csv('applications.csv', index=False, mode='a', header=not file_exists)
+            print("inserted successfully")
+        except KeyboardInterrupt:
+            print("\n---Finished---")
+            time.sleep(1)
+            quit()
+        except Exception as e:
+            print(e)
+            time.sleep(2)
 
 
 if __name__ == '__main__':
